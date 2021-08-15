7ba3d482e54c08e146dc125824fc6c88634c6de3
haotian <wong.haotian@foxmail.com>
2021-08-15 15:03:28 +0800

    update

--- unionfind/src/UF.java
+++ unionfind/src/UF.java
@@ -1,2 +1,44 @@
 public class UF {
+    private int count;
+    private final int[] parent;
+    private final int[] size;
+    public UF(int n){
+        this.count = n;
+        parent = new int[n];
+        size = new int[n];
+        for(int i = 0; i < n; ++i){
+            parent[i] = i;
+            size[i] = i;
+        }
+    }
+    public void union(int p, int q) {
+        int rootP = find(p);
+        int rootQ = find(q);
+        if(rootP == rootQ){
+            return;
+        }
+        if(size[rootP] > size[rootQ]){
+            parent[rootQ] = rootP;
+            size[rootP] = size[rootQ];
+        }else{
+            parent[rootP] = rootQ;
+            size[rootQ] = size[rootP];
+        }
+        count--;
+    }
+    public int find(int x){
+        while(parent[x] != x){
+            parent[x] = parent[parent[x]];
+            x = parent[x];
+        }
+        return x;
+    }
+    public boolean connected(int p, int q){
+        int rootP = find(p);
+        int rootQ = find(q);
+        return rootP == rootQ;
+    }
+    public int count(){
+        return count;
+    }
 }
