5e9c031e8fd2369e11e11946319f0b90e9e61e2d
haotian <wong.haotian@foxmail.com>
2021-08-17 15:22:24 +0800

    update

--- unionfind/src/UF.java
+++ unionfind/src/UF.java
@@ -3,57 +3,18 @@ import edu.princeton.cs.algs4.StdOut;
 
 public class UF {
     private int count;
-    private int[] parent;
+    private int[] id;
     private int[] size;
     public UF(int n){
-        this.count = n;
-        parent = new int[n];
+        count = n;
+        id = new int[n];
         size = new int[n];
-        for(int i = 0; i < n; ++i){
-            parent[i] = i;
+        for (int i = 0; i < n; i++){
+            id[i] = i;
             size[i] = 1;
         }
     }
-    public void union(int p, int q) {
-        int rootP = find(p);
-        int rootQ = find(q);
-        if(rootP == rootQ){
-            return;
-        }
-        if(size[rootP] > size[rootQ]){
-            parent[rootQ] = rootP;
-            size[rootP] += size[rootQ];
-        }else{
-            parent[rootP] = rootQ;
-            size[rootQ] += size[rootP];
-        }
-        count--;
-    }
-    public int find(int x){
-        while(parent[x] != x){
-            parent[x] = parent[parent[x]];
-            x = parent[x];
-        }
-        return x;
-    }
-    public boolean connected(int p, int q){
-        return find(p) == find(q);
-    }
     public int count(){
         return count;
-    }
-    public static void main(String[] args){
-        int n = StdIn.readInt();
-        UF uf = new UF(n);
-        while(!StdIn.isEmpty()){
-            int p = StdIn.readInt();
-            int q = StdIn.readInt();
-            if(uf.connected(p, q)){
-                continue;
-            }
-            uf.union(p, q);
-            StdOut.println(p + "" + q);
-        }
-        StdOut.println(uf.count() + "components");
     }
 }
