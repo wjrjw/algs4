020ede1076bbef48c04bc547aeeb307e9248a7fa
haotian <wong.haotian@foxmail.com>
2021-08-22 16:31:00 +0800

    update

--- demos/unionfind/src/WeightedQuickUnionUF.java
+++ demos/unionfind/src/WeightedQuickUnionUF.java
@@ -1,8 +1,16 @@
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
-
 public class WeightedQuickUnionUF {
-    public int size(int p){
-        return;
+    public int root(int i){
+
+    }
+    public boolean connected(int p, int q){
+        return root(p) == root(q);
+    }
+    public int size(int i){
+        return ;
+    }
+    public static void main(String[] args){
+        int
     }
 }
